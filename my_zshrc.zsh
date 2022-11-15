1e1f29407c42ef285231413491892ebf386e5e3a
Zweih <me@fernandonunez.io>
2022-11-15 16:05:24 -0700

    Revert "Remove whitespace."
    This reverts commit cafd85462a8d77b6a52449f8ab279a30695747b9.

--- my_zshrc.zsh
+++ my_zshrc.zsh
@@ -79,10 +79,6 @@ my_prompt_symbol() {
   echo $(color_text magenta "𐂂   ")
 }
 
-seperator() {
-  echo $(color_text cyan ">")
-}
-
 setopt PROMPT_SUBST
 
 # Load colors
@@ -99,5 +95,5 @@ my_vcs_info() {
   echo $(color_text red "[")$(color_text magenta ${vcs_info_msg_0_})$(color_text red "] ")
 }
 
-PROMPT='$(my_username)$(seperator)$(my_date)$(seperator)$(my_dir)$(seperator)$(my_vcs_info)$(my_prompt_symbol)$(seperator)'
+PROMPT='$(my_username) $(my_date) $(my_dir) $(my_vcs_info)$(my_prompt_symbol) '
 
