06fa1c5d6420579c560cb09530160679ea5b298f
Zweih <15152269+Zweih@users.noreply.github.com>
2022-12-02 17:01:51 -0700

    Remove tab handling. Too laggy.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,26 +3,6 @@ local usb = require "usb"
 hs.ipc.cliInstall()
 usb.init()
 
--- Move to previous tab
-hs.hotkey.bind({"cmd"}, "pageup", function()
-    hs.eventtap.keyStroke({"cmd", "alt"}, "left")
-end)
-
--- Move to next tab
-hs.hotkey.bind({"cmd"}, "pagedown", function()
-    hs.eventtap.keyStroke({"cmd", "alt"}, "right")
-end)
-
--- Rearrange tab left
-hs.hotkey.bind({"cmd, shift"}, "pageup", function()
-    hs.eventtap.keyStroke({"ctrl", "shift"}, "pageup")
-end)
-
--- Rearrange tab right
-hs.hotkey.bind({"cmd, shift"}, "pagedown", function()
-    hs.eventtap.keyStroke({"ctrl", "shift"}, "pagedown")
-end)
-
 hs.hotkey.bind({}, "f13", function()
     os.execute("pmset displaysleepnow")
 end)
